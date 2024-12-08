b7efefd34c86816b7f7883529c22189594da0588
Mohit <mohit.punglia@gmail.com>
2024-12-08 20:04:47 +0000

    adding partial code

--- part1.py
+++ part1.py
@@ -116,3 +116,30 @@ y
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 
+# %%
+from sklearn.linear_model import LinearRegression
+
+# %%
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import r2_score
+
+# %%
+ohe=OneHotEncoder()
+ohe.fit(X[['name','company','fuel_type']])
+
+# %%
+column_trans = make_column_transformer(
+    (OneHotEncoder(categories=ohe.categories_), ["name", "company", "fuel_type"]),
+    remainder="passthrough",
+)
+
+# %%
+lr=LinearRegression()
+
+# %%
+pipe=make_pipeline(column_trans,lr)
+
+# %%
+pipe.fit(X_train,y_train)
